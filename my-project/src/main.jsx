cb44cca35f034802372607bf1827e60048da3e8d
monoronjon sarker <monoronjons86@gmail.com>
2024-07-10 12:10:37 +0600

    more

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import Gallery from './component/Gallery.jsx'
 import Test from './component/Test.jsx'
+import PackingList from './component/PackingList.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Gallery></Gallery>
     <Test></Test>
+    <PackingList></PackingList>
   </React.StrictMode>,
 )
