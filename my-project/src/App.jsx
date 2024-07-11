939f71a628b4329c917df43ca8da31592df7bb22
monoronjon sarker <monoronjons86@gmail.com>
2024-07-11 06:17:41 +0600

    morning add

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,14 +1,44 @@
 
 import './App.css'
+const people = [
+  'Creola Katherine Johnson: mathematician',
+  'Mario José Molina-Pasquel Henríquez: chemist',
+  'Mohammad Abdus Salam: physicist',
+  'Percy Lavon Julian: chemist',
+  'Subrahmanyan Chandrasekhar: astrophysicist'
+];
+
+const peoplee = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',  
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+}];
 
 function App() {
-  
-
+  const listItems=people.map(person=> <li key={person.key}>{person}</li>);
+  const chmist=peoplee.filter(peson=>peson.profession==="chemist");
+  const listItem=chmist.map(chemm=>chemm.name)
   return (
-   
-    <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+    <div>
+      <div>{listItems}</div>
+      <h3>{listItem}</h3>
+    </div>
   )
 }
 
