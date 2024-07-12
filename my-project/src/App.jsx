455bb13069ebd5c9a2193c88beb539b919219e0d
monoronjon sarker <monoronjons86@gmail.com>
2024-07-12 23:10:12 +0600

    ngith

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,25 +1,35 @@
-import Actor from "./Actor";
-import Singer from "./Singer";
+// import Actor from "./Actor";
+// import Singer from "./Singer";
 
-export default function App() {
-  const actors=['Sakib','sorib','tomal','rahim'];
-  const singers=[
-    {name:'Dr mahfuz',age:68},
-    {name:'Eva Rahman',age:28},
-    {name:'Monir khan',age:38},
-    {name:'mahfuz',age:48},
-  ]
-  return (
-    <>
-    {
-      singers.map(singer=><Singer singer={singer}></Singer>)
-    }
-    {/* <Actor name="sakib khan"></Actor> */}
-    {
-      actors.map(actor=><Actor name={actor}></Actor>)
-    }
-    </>
+// export default function App() {
+//   const actors=['Sakib','sorib','tomal','rahim'];
+//   const singers=[
+//     {name:'Dr mahfuz',age:68},
+//     {name:'Eva Rahman',age:28},
+//     {name:'Monir khan',age:38},
+//     {name:'mahfuz',age:48},
+//   ]
+//   return (
+//     <>
+//     {
+//       singers.map(singer=><Singer singer={singer}></Singer>)
+//     }
+//     {/* <Actor name="sakib khan"></Actor> */}
+//     {
+//       actors.map(actor=><Actor name={actor}></Actor>)
+//     }
+//     </>
 
     
+//   )
+// }
+
+export default function App(){
+  return(
+    <>
+     <h3>React core concept 2</h3>
+     <button>Click me</button>
+    </>
   )
+
 }
