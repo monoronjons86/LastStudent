4bbee6089de559c5e13d23c254f434fa927604ad
monoronjon sarker <monoronjons86@gmail.com>
2024-07-12 19:34:44 +0600

    new project

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -2,18 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Gallery from './component/Gallery.jsx'
-import Test from './component/Test.jsx'
-import PackingList from './component/PackingList.jsx'
-import Home from './component/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Gallery></Gallery>
-    <Test></Test>
-    <PackingList></PackingList>
-    <Home></Home>
-    
   </React.StrictMode>,
 )
