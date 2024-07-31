8dc2249769fc073ebd4c8717f366920d079da224
monoronjon sarker <monoronjons86@gmail.com>
2024-07-31 13:22:16 +0600

    router-learn

---
+++ first-router/src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Home/Home';
+import About from './About/About';
+import Contact from './Contact/Contact';
+
+const router=createBrowserRouter([
+  {
+  path:'/',
+  element:<Home></Home>,
+  children:[
+    {
+      path:'about',
+      element:<About></About>
+    },
+    {
+      path:'contact',
+      element:<Contact></Contact>
+    }
+  ]
+},
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+     {/* <App /> */}
+  </React.StrictMode>,
+)
