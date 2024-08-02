7c8ac5fc8b38523d74893dcabcff9cb94fe9b086
monoronjon sarker <monoronjons86@gmail.com>
2024-08-02 16:46:36 +0600

    simple-from

---
+++ my-form/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import SimpleForm from './component/SimpleForm/SimpleForm'
+
+function App() {
+
+
+  return (
+    <>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
