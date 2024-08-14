efea48653de947b06bd08a740125ddd534b2cf9a
monoronjon sarker <monoronjons86@gmail.com>
2024-08-14 09:39:07 +0600

    mmm

--- career-hub/src/Components/FeatureJobs/FeatureJobs.jsx
+++ career-hub/src/Components/FeatureJobs/FeatureJobs.jsx
@@ -1,9 +1,19 @@
 
+// import { useEffect, useState } from "react";
+
 const FeatureJobs = () => {
+    // const {job,setJob}=useState([]);
+    // useEffect(()=>{
+    //     fetch('jobs.json')
+    //     .then(res=>res.json())
+    //     .then(data=>setJob(data))
+    // },[])
+
+
     return (
         <div>
             <div className="text-center">
-                <h2 className="text-5xl">Featured jobs</h2>
+                <h2 className="text-5xl">Featured jobs:</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
         </div>
---
+++ my-project2/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-center text-2xl underline'>Hello bangladesh</h1>
+    </>
+  )
+}
+
+export default App
